c6d7fa3d0f8e8b60b99610feaf8244e3a3ae3804
abhijith <xperio135@gmail.com>
2025-03-14 11:55:20 +0530

    Added content section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './Header';
+import ContentSection from './ContentSection'
 import './App.css';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
-      {/* Rest of your app content */}
+      <ContentSection />
     </div>
   );
 };
